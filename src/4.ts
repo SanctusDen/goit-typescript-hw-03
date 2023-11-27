9b1aa108516c1c6729436e55c2ed63700628f8f7
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-11-27 21:45:58 +0100

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,51 @@
-const key = new Key();
+abstract class House {
+  protected doorIsOpened: boolean;
+  protected key: Key;
+  public abstract openDoor(key: Key): void;
+  public comeIn(tenant: Person): void {}
+}
+class MyHouse extends House {
+  doorIsOpened: boolean = false;
+  private tenants: Person[] = [];
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.doorIsOpened = true;
+    }
+  }
+  public comeIn(tenant: Person): void {
+    if (this.doorIsOpened) {
+      this.tenants.push(tenant);
+    }
+  }
+}
 
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random() * 10;
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
